3588c1d164e638a01a52015c3b87c7c29d45c18b
xuan <anxu5829@gmail.com>
2017-10-28 21:01:39 +0800

    correct SVD method using converCSCmatrix

--- usefulMethod.py
+++ usefulMethod.py
@@ -6,13 +6,10 @@ def selectM(U, list1, list2):
     row = [[i] * len(list2) for i in list1]
     col = np.array(list2 * len(list1)).reshape(len(list1), len(list2))
     return U[row, col]
-
-
 def userAvg(UPmatrix):
     temp = UPmatrix.sum(axis=1) / (UPmatrix != 0).sum(axis=1)
     temp[np.isnan(temp)] = 0
     return temp
-
 def sim(UPmatrix):
     usermean = userAvg(UPmatrix)
     for useri in np.range(UPmatrix.shape[0]):
@@ -35,18 +32,34 @@ def rowCosine(UPmatrix):
                     uu[useri,userj] = temp
 
     return uu
-
-
-
+def converCscMatrix(cscMatrix):
+    datalist = []
+    l = len(cscMatrix.indptr)
+    for i in np.arange(len(cscMatrix.indptr)):
+        if i!=(len(cscMatrix.indptr)-1):
+            datalist.append(cscMatrix.indices[cscMatrix.indptr[i]:cscMatrix.indptr[i+1]])
+    # 用pandas 来解决问题
+    data = pd.DataFrame(columns=["col","row","data"])
+    cols = []
+    for i in np.arange(1,len(cscMatrix.indptr)):
+        cols.extend([i-1]*(cscMatrix.indptr[i]-cscMatrix.indptr[i-1]))
+    data = pd.DataFrame()
+    data["col"] = cols
+    data["row"] = cscMatrix.indices
+    data["data"] = cscMatrix.data
+    return data
 
 def cululateResidual(UPmatrix,UserLatent,ProductLatent):
-    ProductLatentTranspose = ProductLatent.transpose()
-    UPmatrix_hat =UserLatent.dot(ProductLatentTranspose)
-    S = (UPmatrix !=0)
-    Error = UPmatrix-UPmatrix_hat
-    Residual2 = np.multiply(Error, Error)
-    loss = np.sum(np.multiply(S,Residual2))
-    return (Error,loss)
+    data = converCscMatrix(UPmatrix)
+    lengenre = UserLatent.shape[1]
+    Eij = 0
+    for index in data.index:
+        (col,row,value) = data.loc[index]
+        value_hat = 0
+        for k in np.arange(lengenre):
+            value_hat += UserLatent[row,k]*ProductLatent[col,k]
+        Eij += value - value_hat
+    return (Eij)
 
 def SVD(UPmatrix,k,iterator = 10, alpha = 0.005):
     lenOfUser     = UPmatrix.shape[0]
@@ -54,23 +67,24 @@ def SVD(UPmatrix,k,iterator = 10, alpha = 0.005):
     lenOfGenre    = k
     UserLatent    = np.random.random((lenOfUser,lenOfGenre)) #csc_matrix((lenOfUser, lenOfGenre))
     ProductLatent = np.random.random((lenOfProduct,lenOfGenre)) #csc_matrix((lenOfProduct,lenOfGenre))
-    S = (UPmatrix != 0)
+    UserLatent    = csc_matrix(UserLatent)
+    ProductLatent = csc_matrix(ProductLatent)
+    S = converCscMatrix(UPmatrix)
     flag = 0
     while flag < iterator:
-        for user in np.arange(S.shape[0]):
-            for product in np.arange(S.shape[1]):
-                if S[user,product] == 1:
-                    #lossMatrix,_=cululateResidual(UPmatrix,UserLatent,ProductLatent)
-                    #Eij = lossMatrix[user,product]
-                    Eij = 0
-                    for genr in np.arange(k):
-                        Eij = Eij +UserLatent[user,genr]*ProductLatent[product,genr]
-                    Eij = UPmatrix[user,product] - Eij
-                    for genr in np.arange(k):
-                        UserLatentNew=UserLatent[user,genr] + alpha*Eij*ProductLatent[product,genr]
-                        ProductLatentNew = ProductLatent[product,genr] + alpha*Eij*UserLatent[user,genr]
-                        UserLatent[user,genr] = UserLatentNew
-                        ProductLatent[product,genr] = ProductLatentNew
+        for index in S.index:
+            (col, row, value) = S.loc[index]
+            #lossMatrix,_=cululateResidual(UPmatrix,UserLatent,ProductLatent)
+            #Eij = lossMatrix[user,product]
+            value_hat = 0
+            for genr in np.arange(k):
+                value_hat = value_hat +UserLatent[row,genr]*ProductLatent[col,genr]
+            Eij = value - value_hat
+            for genr in np.arange(k):
+                UserLatentNew=UserLatent[row,genr] + alpha*Eij*ProductLatent[col,genr]
+                ProductLatentNew = ProductLatent[col,genr] + alpha*Eij*UserLatent[row,genr]
+                UserLatent[row,genr] = UserLatentNew
+                ProductLatent[col,genr] = ProductLatentNew
         flag+=1
     return (UserLatent,ProductLatent)
 
